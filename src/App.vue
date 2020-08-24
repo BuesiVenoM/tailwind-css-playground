45390829d8cd7c9556e8fac180ef4a863c8b13ed
Sebastian Brunner <sebastian.brunner@jls.ch>
2020-08-24 12:05:30 +0200

    Add finished example

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,37 @@
 <template>
-  <a
-    href="https://markus.oberlehner.net/blog/setting-up-tailwind-css-with-vue/"
-    class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-  >
-    Click me!
-  </a>
+  <div class="py-32 h-128 bg-red">
+    <div class="md:flex bg-white rounded-lg p-6">
+      <div>
+        <img
+          class="h-16 w-16 md:h-24 md:w-24 rounded-full mx-auto md:mx-0 md:mr-6"
+          src="./assets/avatar.png"
+        >
+      </div>
+      <div class="text-center md:text-left">
+        <h2 class="text-lg">
+          Hans Muster
+        </h2>
+        <div class="text-indigo-dark">
+          Flamethrower expert
+        </div>
+        <div class="text-gray-600">
+          hans-get-the@flamethrower.now
+        </div>
+        <div class="text-gray-600">
+          +41 (0)76 407 14 72
+        </div>
+      </div>
+    </div>
+    <div>
+      <div class="text-center text-xs md:text-left">
+        Image copyright by martin-missfeldt.de
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
-  name: `App`,
-};
+  export default {
+    name: `App`,
+  };
 </script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,16 @@
 module.exports = {
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        indigo: {
+          lighter: `#b3bcf5`,
+          default: `#5c6ac4`,
+          dark: `#202e78`,
+        },
+        blue: `#007ace`,
+        red: `#de3618`,
+      },
+    },
   },
   variants: {},
   plugins: [],
